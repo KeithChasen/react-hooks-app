e4ff00f6f09c247e04948bf8ab6cbf49b214d9d0
keithchasen <keithchasen89@gmail.com>
2019-06-19 22:02:32 +0300

    use state hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uuid from 'uuid/v1'
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'Help!', id: 1 },
+        { title: 'Day in the life', id: 2 },
+        { title: 'Lucy in the sky with diamonds', id: 3 },
+    ])
+
+    const addSong = () => {
+        setSongs([...songs, { title: 'new song', id: uuid()}])
+    }
     return (
         <div className="song-list">
             <ul>
-                <li>Song 1</li>
-                <li>Song 2</li>
+                {
+                    songs.map(song => {
+                        return (
+                            <li key={song.id}>
+                                {song.title}
+                            </li>
+                        )
+                    })
+                }
             </ul>
+            <button onClick={addSong}>Add a song</button>
         </div>
     )
 }
